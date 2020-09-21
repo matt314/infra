2467925bfa85bfc0c948aa992e0ea9a05793ab1d
Matt Piechota <piechota@argolis.org>
2020-09-21 12:55:10 -0400

    Add a couple further options.

--- scripts/system-init.sh
+++ scripts/system-init.sh
@@ -3,7 +3,9 @@
 # FIXME just doing fedora for now, should check on uname info and 
 # pick the right package manager
 pkgmgr=dnf
-initpkgs="git ansible"
+initpkgs="git ansible crudini"
 
 sudo $pkgmgr install $initpkgs
+
+# FIXME: git clone; ssh keys?
 
