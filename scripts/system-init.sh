d6d1579b2f29119dac82ef85833af1020d591752
Matt Piechota <mattpie@gmail.com>
2021-12-22 09:15:05 -0500

    Some logic around rpm-ostree, dnf, and apt

--- scripts/system-init.sh
+++ scripts/system-init.sh
@@ -2,10 +2,25 @@
 
 # FIXME just doing fedora for now, should check on uname info and 
 # pick the right package manager
-pkgmgr=dnf
-initpkgs="git ansible crudini"
+if test -x /usr/bin/rpm-ostree ; then
+  echo rpm-ostree not supported yet
+elif test -x /usr/bin/dnf ; then
+  pkgmgr=dnf
+  initpkgs="git ansible crudini"
+elif test -x /usr/bin/apt ; then
+  echo apt not supported yet
+else
+  echo Unable to determine package manager
+  exit 1
+fi
 
-sudo $pkgmgr install $initpkgs
+if test -z ${pkgmgr+x} ; then
+  echo skipping package install
+else
+  # May need to to more checking in here if pkgmgr types need 
+  # different arguments
+  sudo $pkgmgr install $initpkgs
+fi
 
 # set some git config options
 git config --global pull.rebase false
